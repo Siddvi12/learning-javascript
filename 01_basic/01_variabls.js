551c401c6dc7448342a1e3136bd80609d89d4f8a
Vikash Singh <138220005+Siddvi12@users.noreply.github.com>
2023-10-13 19:18:51 +0530

    till datatypes

---
+++ 01_basic/01_variabls.js
@@ -0,0 +1,25 @@
+const accountId = 14452;
+let accountEmail ="myanyloc@gmail.com";
+var accountPassword ="0123456"
+let accountCity = "lko";
+let accountState 
+
+// accountId =12    // not allowed
+console.log(accountId);
+
+accountPassword = "254685"
+
+accountCity="blor"
+console.log(accountCity);
+
+accountEmail="766myany@gmail.com"
+console.log(accountEmail);
+
+
+// but instade of useing console.log() we will use 
+
+console.table([accountCity, accountEmail, accountId, accountPassword, accountState]);
+
+/**
+ * never use var because of scope-----------
+ */
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,34 @@
+// datatype
+
+"use strict" ; // treat all JS as newer version
+
+// alert("hello");  we are using node not browser
+
+// we must write readable code 
+
+console.log(3+3);
+
+let namee ="vikash";
+let age = 25;
+let isLoggedIn = false;
+
+console.table([namee,age,isLoggedIn])
+
+/**
+ * number
+ * bigint
+ * string
+ * boolean
+ * null = stand alone value
+ * undefined = value not defined
+ * symbols => unique
+ */
+
+// objects
+
+console.log(typeof namee);
+
+// null is a object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
