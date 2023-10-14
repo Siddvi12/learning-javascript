f3f45f0d9c8f1f2b41e433aa18d17dde0dede698
Vikash Singh <138220005+Siddvi12@users.noreply.github.com>
2023-10-14 12:09:38 +0530

    datatypes and conversion

---
+++ 01_basic/03_datatype-and-conversion.js
@@ -0,0 +1,16 @@
+let score = "33avb";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+//to convert in number
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);  // NaN
+
+// "33" => 33
+//33asd => nan
+//true =1
+// false =0
+
