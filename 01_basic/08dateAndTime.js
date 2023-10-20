08750567e4aaa4535216fe96a9f6220928de67ac
Vikash Singh <138220005+Siddvi12@users.noreply.github.com>
2023-10-20 16:19:20 +0530

    till dates

---
+++ 01_basic/08dateAndTime.js
@@ -0,0 +1,22 @@
+// dates 
+// date is an object
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+//only date
+
+console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+
+//date and time 
+console.log(myDate.toLocaleString());
+
+// times ===============quzis like me jarurat pdta hai
+
+
+
+
+
